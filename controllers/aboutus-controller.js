5ae36a31d8ed81ca475696bf19457e76422e9cc6
Bogdan <bogdan.lebedenko@gmail.com>
2016-03-29 17:32:31 +0300

    Created about us page, added info in controller, also changes with contact us page template.

--- controllers/aboutus-controller.js
+++ controllers/aboutus-controller.js
@@ -1,6 +1,6 @@
 /*
  *
- * Details Controller
+ * About Us Controller
  * By Lebedenko Bogdan
  * 27/03/2016
  *
@@ -8,6 +8,25 @@
 
 myApp.controller('aboutUsController', function($scope) {
 
+    $scope.author = {
+        name: 'Bogdan',
+        surname: 'Lebedenko',
+        position: 'Front-end Developer'
+    };
 
+    $scope.skills = [
+        {name: 'HTML/CSS', percent: 80},
+        {name: 'JavaScript', percent: 30},
+        {name: 'Design', percent: 70},
+        {name: 'PHP', percent: 30},
+        {name: 'Wordpress', percent: 60},
+        {name: 'LESS/SAAS', percent: 80},
+        {name: 'jQuery', percent: 50},
+        {name: 'Bootstrap', percent: 80},
+        {name: 'Foundation', percent: 80},
+        {name: 'MySQL', percent: 25},
+        {name: 'Jira', percent: 40},
+        {name: 'Git', percent: 40}
+    ];
 
 });
--- controllers/contactus-controller.js
+++ controllers/contactus-controller.js
@@ -8,6 +8,10 @@
 
 myApp.controller('contactUsController', function($scope) {
 
-
+    $scope.contacts = {
+        email: 'bogdan.lebedenko@gmail.com',
+        phone: '+38 093 26 76 494',
+        skype: 'bogdan.lebedenko'
+    }
 
 });
